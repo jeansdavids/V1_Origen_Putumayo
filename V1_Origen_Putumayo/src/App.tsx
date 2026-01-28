d52be4d682f001b9cf1e5bea6dc3b6b2ca5efaa7
Kaleth2216 <acamacho-2022a@corhuila.edu.co>
2026-01-27 23:05:04 -0500

    feat(cart): carrito de compras añadido en navbar y products

--- V1_Origen_Putumayo/src/App.tsx
+++ V1_Origen_Putumayo/src/App.tsx
@@ -3,18 +3,22 @@ import { Routes, Route } from "react-router-dom";
 import NavbarLayout from "./components/common/NavbarLayout";
 import Home from "./pages/public/Home";
 import History from "./pages/public/History";
-
 import Products from "./pages/public/Products";
+import { CartProvider } from "./features/cart/CartContext";
+import CartDrawer from "./features/cart/CartDrawer";
 
 const App: React.FC = () => {
   return (
+    <CartProvider>
     <NavbarLayout>
+      <CartDrawer />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/history" element={<History />} />
         <Route path="/products" element={<Products />} />
       </Routes>
     </NavbarLayout>
+    </CartProvider>
   );
 };
 
