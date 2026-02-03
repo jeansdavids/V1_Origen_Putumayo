244870231ac0ac07c02bd09520bd7d8659deaac9
kaleth2216 <calethcamacho@gmail.com>
2026-02-03 17:08:23 -0500

    Cambio de posicion del boton hamburguesa y cambio de iconos del boton hamburguesa y carrito de compras

--- V1_Origen_Putumayo/src/components/common/Navbar.tsx
+++ V1_Origen_Putumayo/src/components/common/Navbar.tsx
@@ -1,10 +1,9 @@
 import "../../styles/Navbar.css";
+import "../../styles/navbarcel.css";
 import { Link } from "react-router-dom";
 import { useCart } from "../../features/cart/CartContext";
 import logo from "../../assets/images/logo.png";
 import { useState } from "react";
-import "../../styles/navbarcel.css"
-
 
 const Navbar = () => {
   const { totalItems, openCart } = useCart();
@@ -16,17 +15,6 @@ const Navbar = () => {
       <div className="navbar-logo">
         <img src={logo} alt="Logo" />
       </div>
-
-      {/* Hamburguesa */}
-      <button
-        className="navbar-toggle"
-        onClick={() => setMenuOpen(!menuOpen)}
-        aria-label="Abrir menú"
-      >
-        <span />
-        <span />
-        <span />
-      </button>
 
       {/* Menu */}
       <ul className={`navbar-menu ${menuOpen ? "active" : ""}`}>
@@ -47,33 +35,33 @@ const Navbar = () => {
         </li>
       </ul>
 
-      {/* Carrito */}
-      <button
-        className="navbar-cartBtn"
-        type="button"
-        aria-label="Abrir carrito"
-        onClick={openCart}
-      >
-        <svg
-          className="navbar-cartIcon"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
+      {/* Acciones derecha: hamburguesa + carrito */}
+      <div className="navbar-actions">
+        {/* Hamburguesa */}
+        <button
+          className="navbar-toggle"
+          onClick={() => setMenuOpen(!menuOpen)}
+          aria-label="Abrir menú"
         >
-          <circle cx="9" cy="21" r="1" />
-          <circle cx="20" cy="21" r="1" />
-          <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6" />
-        </svg>
+          <i className="bi bi-list"></i>
+        </button>
 
-        {totalItems > 0 && (
-          <span className="navbar-cartBadge">{totalItems}</span>
-        )}
-      </button>
+        {/* Carrito */}
+        <button
+          className="navbar-cartBtn"
+          type="button"
+          aria-label="Abrir carrito"
+          onClick={openCart}
+        >
+          <i className="bi bi-cart3"></i>
+
+          {totalItems > 0 && (
+            <span className="navbar-cartBadge">{totalItems}</span>
+          )}
+        </button>
+      </div>
     </nav>
   );
 };
+
 export default Navbar;
