d816e95b64bd4b52b1c36e13cdc1171216273a1d
kaleth2216 <calethcamacho@gmail.com>
2026-02-12 23:23:01 -0500

    Feat: Correción de bug de las pestañas

--- V1_Origen_Putumayo/src/App.tsx
+++ V1_Origen_Putumayo/src/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import NavbarLayout from "./components/common/NavbarLayout";
 import FooterLayout from "./components/common/FooterLayout";
 import Home from "./pages/public/Home";
@@ -11,8 +11,13 @@ import { CartProvider } from "./features/cart/CartContext";
 import CartDrawer from "./features/cart/CartDrawer";
 import Contacto from "./pages/public/Contacto";
 
-
 const App: React.FC = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <CartProvider>
       <NavbarLayout>
