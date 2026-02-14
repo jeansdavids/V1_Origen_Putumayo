f84419fe3a5401307d7ea9ad08260c9cf4bf0b35
kaleth2216 <calethcamacho@gmail.com>
2026-02-13 21:44:48 -0500

    feat: Mejora de iconos

---
+++ V1_Origen_Putumayo/src/lib/icons.ts
@@ -0,0 +1,21 @@
+import {
+  faShoppingCart,
+  faHeart,
+  faUser,
+  faBars,
+  faMinus,
+  faPlus,
+  faTrash,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
+
+export const Icons = {
+  cart: faShoppingCart,
+  heart: faHeart,
+  user: faUser,
+  menu: faBars,
+  minus: faMinus,
+  plus: faPlus,
+  trash: faTrash,
+  close: faXmark,
+};
